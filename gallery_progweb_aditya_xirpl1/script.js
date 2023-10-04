c8b851b6e4976057df32586e60a08854ece31259
fnticxdesign <142726822+fnticxdesign@users.noreply.github.com>
2023-10-04 23:02:37 +0700

    Add files via upload

---
+++ gallery_progweb_aditya_xirpl1/script.js
@@ -0,0 +1,13 @@
+const type = new Typed(".typing", {
+    strings: ["adityaa.", "xirpl1", "pacar amel", "designer"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true,
+});
+
+const typi = new Typed(".typo", {
+    strings: ["my gallery", "our memories", "collections", "photos"],
+    typeSpeed: 200,
+    backSpeed: 100,
+    loop: true,
+});
